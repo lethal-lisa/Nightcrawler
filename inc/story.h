c9d65146f8f7b7edd9414a9661385bcf5656b268
Lisa Murray <k4mu1kazum4@gmail.com>
2022-09-30 19:07:12 -0500

    Shortened uInitialSceneAddr to uInitSceneAddr.

--- inc/story.h
+++ inc/story.h
@@ -18,7 +18,7 @@ struct storyFileHdr
 	uint32_t uGameTitleAddr; // Address in the file where game title string begins.
 	uint16_t uMaskUsedItems; // Mask of used items (also used as a count of the items).
 	uint32_t uItemNameAddr; // Address in the file where item name cluster begins.
-	uint32_t uInitialSceneAddr; // Address in the file where the file name for the initial scene node is found.
+	uint32_t uInitSceneAddr; // Address in the file where the file name for the initial scene node is found.
 };
 
 inline FILE *openStoryFile (const char *pszFileName);
