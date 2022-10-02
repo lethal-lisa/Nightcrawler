97e8fa835cafc6ba7700e1d5cb082047fd19d746
Lisa Murray <k4mu1kazum4@gmail.com>
2022-10-01 23:42:49 -0500

    Updated scene header & wrote out stuff in the example story.

--- inc/scene.h
+++ inc/scene.h
@@ -16,4 +16,23 @@ typedef struct tagSceneNodeHdr
 	uint32_t uUseClustAddr; // USE cluster address.
 } __attribute((packed, aligned(4))) sceneNodeHdr;
 
+typedef struct tagScene_MoveCluster
+{
+	char szMagic[4]; // Magic number identifier "MOV".
+	uint32_t uNorthAddr; // Scene node to the North.
+	uint32_t uSouthAddr; // Scene node to the South.
+	uint32_t uEastAddr; // Scene node to the East.
+	uint32_t uWestAddr; // Scene node to the West.
+} __attribute((packed, aligned(4))) scene_MoveCluster;
+
+typedef struct tagScene_LookCluster
+{
+	char szMagic[4]; // Magic number ID "LOK".
+	uint32_t uAroundAddr; // Look around str addr.
+	uint32_t uNorthAddr; // Look North str addr.
+	uint32_t uSouthAddr; // Look South str addr.
+	uint32_t uEastAddr; // Look East str addr.
+	uint32_t uWestAddr; // Look West str addr.
+} __attribute((packed, aligned(4))) scene_LookCluster;
+
 #endif /* __SCENE_H__ */
