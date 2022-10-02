e7f1c158004400efe6c1393b754ae51d61d0e214
Lisa Murray <k4mu1kazum4@gmail.com>
2022-10-02 04:02:45 -0500

    Added additional structures to scene header to flesh out subnodes.

--- inc/scene.h
+++ inc/scene.h
@@ -35,4 +35,22 @@ typedef struct tagScene_LookCluster
 	uint32_t uWestAddr; // Look West str addr.
 } __attribute((packed, aligned(4))) scene_LookCluster;
 
+typedef struct tagScene_TalkCluster
+{
+	char szMagic[4]; // Magic "TLK".
+	uint16_t cDiaNodes; // Count of DIA nodes.
+} __attribute((packed, aligned(4))) scene_TalkCluster;
+
+typedef struct tagTalk_DiaNode
+{
+	char szMagic[4]; // Magic "DIA".
+	uint16_t cOpts; // Count of OPT nodes (if any).
+	uint16_t fItems; // Item flags to set.
+	uint16_t fStory; // Story flags to set.
+	uint16_t fReqItems; // Required item flags.
+	uint16_t fReqStory; // Required story flags.
+	uint32_t uTextAddr; // Address of primary text.
+	uint32_t uAltTextAddr; // Address of text to display when Req'd flags not met.
+} __attribute((packed, aligned(4))) talk_DiaNode;
+
 #endif /* __SCENE_H__ */
