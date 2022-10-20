897fa5b3832f0ec499597efb0eee2526b851342d
Lisa Murray <k4mu1kazum4@gmail.com>
2022-10-20 16:48:36 -0500

    Replaced an erroneous semicolon in a comment with a period.

--- inc/story.h
+++ inc/story.h
@@ -38,7 +38,7 @@ typedef struct tagSceneNodeHdr
 	char szMagic[4]; // Magic number identifier "NSC".
 	uint32_t uMoveClustAddr; // MOVE cluster address.
 	uint32_t uLookClustAddr; // LOOK cluster address.
-	uint16_t uGetMask; // GET item mask;
+	uint16_t uGetMask; // GET item mask.
 	uint32_t uTalkClustAddr; // TALK cluster address.
 	uint32_t uUseClustAddr; // USE cluster address.
 } __attribute((packed)) sceneNodeHdr;
